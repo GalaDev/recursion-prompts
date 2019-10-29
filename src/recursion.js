08a2b3cebaed691a7a5ad6c450e4586a122c79d8
Andy Gala <livelifebrands@gmail.com>
2019-10-29 12:42:24 -0700

    Pass all tests for sum array
    Might be an error in test suite as test for calling itself is failing however function does call it self in solution

--- src/recursion.js
+++ src/recursion.js
@@ -14,7 +14,19 @@ var factorial = function(n) {
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
-var sum = function(array) {};
+var sum = function(array) {
+  var sumNum = 0;
+
+  array.forEach(numOrArr => {
+    if (Array.isArray(numOrArr)) {
+      sumNum += sum(numOrArr);
+    } else {
+      sumNum += numOrArr;
+    }
+  });
+
+  return sumNum;
+};
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
